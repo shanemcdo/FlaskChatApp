aada9f402010caedbaee64e20d402fcb43c17e62
Shane McDonough <shanemcdman@gmail.com>
2021-01-05 03:54:20 -0800

    Create handle_message

--- chat_app.py
+++ chat_app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY # randomly generated code
 socketio = SocketIO(app)
 
+@socketio.on('message')
+def handle_message(message):
+    """Handle receiving messages"""
+    print(f'\t[[New Message]]: message')
+    send(message, broadcast = True)
+
 @app.route('/')
 def index():
     """Serve the main page of the website"""
