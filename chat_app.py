1efa7e1c2aaa20df5f80d78dbc515575c5ae01ff
Shane McDonough <shanemcdman@gmail.com>
2021-01-05 03:50:43 -0800

    Use secret key in another file

--- chat_app.py
+++ chat_app.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, render_template, url_for
 from flask_socketio import SocketIO, send
+from my_secrets import SECRET_KEY
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY # randomly generated code
 socketio = SocketIO(app)
 
 @app.route('/')
